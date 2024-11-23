2b8d4c0fc85d66e870b1a10b2b7a60bea05f283e
NachoNeko2 <z196482963@163.com>
2024-11-23 20:58:23 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,39 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '命运澪网站监视',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm798079134-3301a3a2681604e286ecf043',
+    'm798079141-a377cd5dcfa50d0858754cab',
+    'm798079126-c97e50fbb398fa92bb000cfa',
+    'm798079128-4232a9cf30a5b87455e9bbbb',
+    'm798079134-3301a3a2681604e286ecf043',
+    'm798079129-9704b4f02aca6480d76250a8',
+    'm798079142-dd23a37241a6630409d44338',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://zt.fate-tec.top/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/NachoNeko2'
+    },
+    {
+      text: 'Blog',
+      url: 'https://blog.fate-tec.top/'
+    },
+  ],
+};
