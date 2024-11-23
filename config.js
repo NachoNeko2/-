050aa5f414be5615bc3a5afa0d72e6cba02c4b32
NachoNeko2 <z196482963@163.com>
2024-11-23 21:14:31 +0800

    Update config.js

--- config.js
+++ config.js
@@ -6,14 +6,13 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm798079124-a8d8b7fec9eff7fea2711ca4',
-    'm798079134-3301a3a2681604e286ecf043',
-    'm798079141-a377cd5dcfa50d0858754cab',
-    'm798079126-c97e50fbb398fa92bb000cfa',
-    'm798079128-4232a9cf30a5b87455e9bbbb',
-    'm798079134-3301a3a2681604e286ecf043',
-    'm798079129-9704b4f02aca6480d76250a8',
     'm798079142-dd23a37241a6630409d44338',
+    'm798079141-a377cd5dcfa50d0858754cab',
+    'm798079129-9704b4f02aca6480d76250a8',
+    'm798079134-3301a3a2681604e286ecf043',
+    'm798079124-a8d8b7fec9eff7fea2711ca4',
+    'm798079128-4232a9cf30a5b87455e9bbbb',
+    'm798079126-c97e50fbb398fa92bb000cfa',
   ],
 
   // 日志天数
